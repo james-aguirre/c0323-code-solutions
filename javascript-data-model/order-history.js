9be71b611d51b9c18e35ec060bdc22fe66bdc549
James Aguirre <jamesaguirre98@gmail.com>
2023-03-21 21:13:42 +0000

    updated all property names to camelCase and removed quotes from property names

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,42 +1,42 @@
 
 var orderHistory = [{
-  'Order #1': 'July 3, 2020',
+  orderNumber1: 'July 3, 2020',
   isbn: '978-123483424',
   title: 'The Art of Sql',
   author: 'Faroult, Stephane',
   isbn2: '979-2314534534',
   item: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
-  Manufacturer: 'Nintendo Inc',
-  'ORDER#': '113-2882177-2648248',
-  TOTAL: '$138.93',
-  Delivered: 'July 5, 2020'
+  manufacturer: 'Nintendo Inc',
+  orderNumber: '113-2882177-2648248',
+  total: '$138.93',
+  delivered: 'July 5, 2020'
 },
 {
-  'Order#2': 'July 7, 2020',
+  orderNumber2: 'July 7, 2020',
   isbn: '979-2345635426',
   manufacturer: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter',
   author: 'Nintendo Inc',
-  'ORDER#': '114-2875557-9059409',
-  TOTAL: '$17.22',
-  Delivered: 'July 7, 2020'
+  orderNumber: '114-2875557-9059409',
+  total: '$17.22',
+  delivered: 'July 7, 2020'
 },
 {
-  'Order #3': 'July 19, 2020',
+  orderNumber3: 'July 19, 2020',
   isbn: '978-123456677',
   title: 'The Timeless Way of Building',
   author: 'Alexander, Christopher',
-  'ORDER#': '113-9984268-1280257',
-  TOTAL: '$44.53',
-  Delivered: 'July 20, 2020'
+  orderNumber: '113-9984268-1280257',
+  total: '$44.53',
+  delivered: 'July 20, 2020'
 },
 {
-  'Order #4': 'August 4, 2020',
+  orderNumber4: 'August 4, 2020',
   isbn: '979-666664531',
   title: 'Javascript for Impatient Programmers',
   author: 'Rauschmayer, Dr. Axel',
-  'ORDER#': '114-3941689-8772232',
-  TOTAL: '$34.00',
-  Delivered: 'August 8, 2020'
+  orderNumber: '114-3941689-8772232',
+  total: '$34.00',
+  delivered: 'August 8, 2020'
 }
 
 ];
