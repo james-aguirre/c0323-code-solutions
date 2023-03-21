185791d32b694a6a80b6eea2aa3c7c87253acba2
James Aguirre <jamesaguirre98@gmail.com>
2023-03-21 18:59:49 +0000

    switched values of bookshelf.js to order-history.js

---
+++ javascript-data-model/bookshelf.js
@@ -0,0 +1,42 @@
+var bookshelf = [
+  {
+    isbn: '978-1491929483',
+    title: 'Introduction to JavaScript Object Notation: A To-the-Point Guide to JSON',
+    author: 'Lindsay Bassett'
+  },
+  {
+    isbn: '978-1484218624',
+    title: 'JSON Quick Syntax Reference',
+    author: 'Wallace Jackson'
+  },
+  {
+    isbn: '978-0692232699',
+    title: 'Build APIs You Won\'t Hate: Everyone and their dog wants an API, so you should probably learn how to build them',
+    author: 'Phil Sturgeon & Laura Bohill'
+  },
+  {
+    isbn: '978-123483424',
+    title: 'The Art of Sql',
+    author: 'Faroult, Stephane'
+  },
+  {
+    isbn: '979-2314534534',
+    title: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+    author: 'Nintendo Inc'
+  },
+  {
+    isbn: '979-2345635426',
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter',
+    author: 'Nintendo Inc'
+  },
+  {
+    isbn: '978-123456677',
+    title: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher'
+  },
+  {
+    isbn: '979-666664531',
+    title: 'Javascript for Impatient Programmers',
+    author: 'Rauschmayer, Dr. Axel'
+  }
+];
---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,44 @@
+
+var orderHistory = [{
+  'Order #1': 'July 3, 2020',
+  isbn: '978-123483424',
+  title: 'The Art of Sql',
+  author: 'Faroult, Stephane',
+  isbn2: '979-2314534534',
+  item: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+  Manufacturer: 'Nintendo Inc',
+  'ORDER#': '113-2882177-2648248',
+  TOTAL: '$44.53',
+  Placed: 'July 3, 2020',
+  Delivered: 'July 5, 2020'
+},
+{
+  'Order#2': 'July 7, 2020',
+  isbn: '979-2314534534',
+  title: 'GameCube Controller - Super Smash Bros. Edition(Nintendo Switch)',
+  Manufacturer: 'Nintendo Inc',
+  'ORDER#': '114-2875557-9059409',
+  TOTAL: '$17.22',
+  Delivered: 'July 7, 2020'
+},
+{
+  'Order #3': 'July 19, 2020',
+  isbn: '978-123456677',
+  title: 'The Timeless Way of Building',
+  author: 'Alexander, Christopher',
+  'ORDER#': '113-9984268-1280257',
+  TOTAL: '$44.53',
+  Delivered: 'July 20, 2020'
+},
+{
+  'Order #4': 'August 4, 2020',
+  isbn: '979-666664531',
+  title: 'Javascript for Impatient Programmers',
+  author: 'Rauschmayer, Dr. Axel',
+  'ORDER#': '114-3941689-8772232',
+  TOTAL: '$34.00',
+  Delivered: 'August 8, 2020'
+}
+
+];
+console.log('Value of orderHistory is ', orderHistory);
