89cb03fc802c60c364891e3df8ffd0a08b45b802
James Aguirre <jamesaguirre98@gmail.com>
2023-03-23 22:46:38 +0000

    finished activity

---
+++ string-manipulation-1/src/get-last-char.js
@@ -0,0 +1,5 @@
+/* exported getLastChar */
+function getLastChar(string) {
+  const lastChar = string.length - 1;
+  return string[lastChar];
+}
---
+++ string-manipulation-1/src/is-vowel.js
@@ -0,0 +1,9 @@
+/* exported isVowel */
+function isVowel(character) {
+  if (character === 'a' || character === 'e' || character === 'i' || character === 'o' || character === 'u' ||
+  character === 'A' || character === 'E' || character === 'I' || character === 'O' || character === 'U') {
+    return true;
+  } else {
+    return false;
+  }
+}
