e9652086041ba2417394e46415bdef339945c404
James Aguirre <jamesaguirre98@gmail.com>
2023-03-28 18:36:22 +0000

    ..

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,8 +1,12 @@
 /* exported toObject */
-function toObject(keyValuepair) {
-  const obj = Object.create(keyValuepair);
-  let values = {};
-  for (const key in obj) {
-    values = [obj] + [key];
-  } return values;
+// function toObject(keyValuepair) {
+//  const obj = Object.create(keyValuepair);
+//  let values = {};
+//  for (const key in obj) {
+//    values = [obj] + [key];
+//  } return values;
+// }
+function toObject(keyValuePair) {
+  const obj = {} + keyValuePair;
+  return obj;
 }
