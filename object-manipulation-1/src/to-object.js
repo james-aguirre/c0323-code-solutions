25296b51bbbed653519e9e371ccb67e512027e46
James Aguirre <jamesaguirre98@gmail.com>
2023-03-28 22:34:14 +0000

    switching fr now

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,25 +1,6 @@
 /* exported toObject */
-// function toObject(keyValuepair) {
-//  const obj = Object.create(keyValuepair);
-//  let values = {};
-//  for (const key in obj) {
-//    values = [obj] + [key];
-//  } return values;
-// }
-// function toObject(keyValuePair) {
-//  const obj = Object.create(keyValuePair);
-//  const newObj = {};
-//  for (const key in obj) {
-//    newObj.key = obj[key];
-//  } return newObj;
-// }
-// function toObject(keyValuePair) {
-//  let obj = {};
-//  for (let i = 0; i < keyValuePair.length; i++) {
-//    obj = keyValuePair[i];
-//  } return obj;
-// }
 function toObject(keyValuePair) {
-  const result = keyValuePair.reduce(((obj, cur) => ({ ...obj, [cur.sid]: cur }), {}));
-  return result;
+  const object = {};
+  object[keyValuePair[0]] = keyValuePair[1];
+  return object;
 }
