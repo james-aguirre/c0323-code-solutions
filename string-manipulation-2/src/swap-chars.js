427cb316eb503b31a54114345b38bc28a51d4942
James Aguirre <jamesaguirre98@gmail.com>
2023-03-29 21:26:14 +0000

    in progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,5 +1,4 @@
 /* exported swapChars */
 function swapChars(firstIndex, secondIndex, string) {
-  string.charAt[firstIndex] = string.charAt[secondIndex];
 }
 console.log(swapChars(0, 1, 'lodash'));
