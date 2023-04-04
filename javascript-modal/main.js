b9a0020f7a6ebc6b44b7a87f4187537fbbd0e6df
James Aguirre <jamesaguirre98@gmail.com>
2023-04-04 20:58:16 +0000

    javascript-modal

---
+++ javascript-modal/main.js
@@ -0,0 +1,22 @@
+let count = 0;
+const $modalButton = document.querySelector('.open-modal');
+const $modal = document.querySelector('.modal');
+$modalButton.addEventListener('click', handleClick);
+const $modalExit = document.querySelector('.no');
+$modalExit.addEventListener('click', exitModal);
+const $body = document.querySelector('.light');
+
+function handleClick(event) {
+  count++;
+  if (count > 0) {
+    $modal.className = 'modal-click';
+    $body.className = 'dark';
+  }
+}
+function exitModal(event) {
+  count--;
+  if (count === 0) {
+    $modal.className = 'modal';
+    $body.className = 'light';
+  }
+}
