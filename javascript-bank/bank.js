43085bcf6981ae45978a50cc73b65d9526607248
James Aguirre <jamesaguirre98@gmail.com>
2023-04-19 04:13:18 +0000

    .

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -5,11 +5,21 @@ function Bank() {
 }
 
 Bank.prototype.openAccount = function (holder, balance) {
-  if (balance > 0) {
+  if (balance > 0 && typeof balance === 'number' && balance === Math.round(balance)) {
     const account = new Account(this.nextAccountNumber, balance);
     account.deposit(balance);
     this.accounts.push(account);
-    this.nextAccountNumber++;
-    return account.holder;
+    this.nextAccountNumber += 1;
+    return account.number;
+  } else {
+    return null;
   }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (let i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
+    }
+  } return null;
 };
