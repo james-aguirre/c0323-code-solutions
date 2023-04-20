cb33bdaca653810220eff7f34a05fd631d01b3f7
James Aguirre <jamesaguirre98@gmail.com>
2023-04-20 03:54:10 +0000

    js bank

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -8,6 +8,7 @@ Bank.prototype.openAccount = function (holder, balance) {
   if (balance > 0 && typeof balance === 'number' && balance === Math.round(balance)) {
     const account = new Account(this.nextAccountNumber, balance);
     account.deposit(balance);
+    account.holder = holder;
     this.accounts.push(account);
     this.nextAccountNumber += 1;
     return account.number;
@@ -22,4 +23,16 @@ Bank.prototype.getAccount = function (number) {
       return this.accounts[i];
     }
   } return null;
+};
+
+Bank.prototype.getTotalAssets = function () {
+  let total = 0;
+  if (this.accounts === undefined) {
+    return 0;
+  } else {
+    for (let i = 0; i < this.accounts.length; i++) {
+      total += this.accounts[i].getBalance();
+    }
+  }
+  return total;
 };
