243900103be16b9c7a4187bb7cc3b068bfe15a40
James Aguirre <jamesaguirre98@gmail.com>
2023-04-20 05:12:07 +0000

    js carousel

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -1,5 +1,4 @@
 // global dom variables
-const $container = document.querySelector('.container');
 const $leftButton = document.querySelector('.fa-chevron-left');
 const $rightButton = document.querySelector('.fa-chevron-right');
 const $buttonOne = document.querySelector('#one');
@@ -9,21 +8,25 @@ const $buttonFour = document.querySelector('#four');
 const $buttonFive = document.querySelector('#five');
 const $test = document.querySelectorAll('.img');
 const $buttonNodes = document.querySelectorAll('i');
-const $carouselButtons = document.querySelector('.corousel-progress');
-console.log($carouselButtons);
-console.log($container);
+
 let count = 0;
 let countDownId = null;
 // data for entries
 
 $rightButton.addEventListener('click', function (event) {
-  viewSwap(count + 1);
-  console.log('effective');
+  if (count >= 4) {
+    count = 0;
+    viewSwap(count);
+  }
 });
 
 $leftButton.addEventListener('click', function (event) {
-  viewSwap(count - 1);
-  console.log('effective');
+  if (count <= 0) {
+    count = 4;
+    viewSwap(count);
+  } else {
+    viewSwap(count - 1);
+  }
 });
 
 $buttonOne.addEventListener('click', function (event) {
@@ -35,30 +38,61 @@ $buttonOne.addEventListener('click', function (event) {
     }
     event.target.classList.add('fa-solid');
     event.target.classList.remove('fa-regular');
-    console.log(event.target);
   }
 });
 
 $buttonTwo.addEventListener('click', function (event) {
   viewSwap(1);
+  for (let i = 0; i < $buttonNodes.length; i++) {
+    if ($buttonNodes[i].hasAttribute('id')) {
+      $buttonNodes[i].classList.remove('fa-solid');
+      $buttonNodes[i].classList.add('fa-regular');
+    }
+    event.target.classList.add('fa-solid');
+    event.target.classList.remove('fa-regular');
+  }
 });
 
 $buttonThree.addEventListener('click', function (event) {
   viewSwap(2);
+  for (let i = 0; i < $buttonNodes.length; i++) {
+    if ($buttonNodes[i].hasAttribute('id')) {
+      $buttonNodes[i].classList.remove('fa-solid');
+      $buttonNodes[i].classList.add('fa-regular');
+    }
+    event.target.classList.add('fa-solid');
+    event.target.classList.remove('fa-regular');
+  }
 });
 
 $buttonFour.addEventListener('click', function (event) {
   viewSwap(3);
+  for (let i = 0; i < $buttonNodes.length; i++) {
+    if ($buttonNodes[i].hasAttribute('id')) {
+      $buttonNodes[i].classList.remove('fa-solid');
+      $buttonNodes[i].classList.add('fa-regular');
+    }
+    event.target.classList.add('fa-solid');
+    event.target.classList.remove('fa-regular');
+  }
 });
 
 $buttonFive.addEventListener('click', function (event) {
   viewSwap(4);
+  for (let i = 0; i < $buttonNodes.length; i++) {
+    if ($buttonNodes[i].hasAttribute('id')) {
+      $buttonNodes[i].classList.remove('fa-solid');
+      $buttonNodes[i].classList.add('fa-regular');
+    }
+    event.target.classList.add('fa-solid');
+    event.target.classList.remove('fa-regular');
+  }
 }
 );
 function swap() {
-  count++;
   if ($test.length > count) {
     viewSwap(count);
+    count++;
   } else {
     count = 0;
   }
@@ -77,6 +111,14 @@ function viewSwap(number) {
       $buttonNodes[i].classList.remove('fa-solid');
       $buttonNodes[i].classList.add('fa-regular');
     }
+  }
+  if ($buttonNodes[count + 2] > 6) {
+    count = 0;
+    $buttonNodes[count + 2].classList.add('fa-solid');
+    $buttonNodes[count + 2].classList.remove('fa-regular');
+  } else {
+    $buttonNodes[count + 2].classList.add('fa-solid');
+    $buttonNodes[count + 2].classList.remove('fa-regular');
 
   }
   clearInterval(countDownId);
