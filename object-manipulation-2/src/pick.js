09ecd4b6972a882fde2ff0951a40c2931926014a
James Aguirre <jamesaguirre98@gmail.com>
2023-05-03 22:56:12 +0000

    pick function completed

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,7 +1,12 @@
 /* exported pick */
 function pick(source, keys) {
-  const obj = {};
+  const newObj = {};
   for (const key in source) {
-    obj[keys] = source[key];
-  }
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i] === key && source[key] !== undefined) {
+        newObj[keys[i]] = source[key];
+        newObj[key] = source[key];
+      }
+    }
+  } return newObj;
 }
