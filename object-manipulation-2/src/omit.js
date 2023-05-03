19cc77d777bd7836184bb43bcc576ba0e2f06958
James Aguirre <jamesaguirre98@gmail.com>
2023-05-03 23:11:15 +0000

    omit function complete

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -1,1 +1,12 @@
 /* exported omit */
+function omit(source, keys) {
+  const newObj = {};
+  for (const key in source) {
+    newObj[key] = source[key];
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i] === key && source[key] !== undefined) {
+        delete newObj[key];
+      }
+    }
+  } return newObj;
+}
--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -4,7 +4,6 @@ function pick(source, keys) {
   for (const key in source) {
     for (let i = 0; i < keys.length; i++) {
       if (keys[i] === key && source[key] !== undefined) {
-        newObj[keys[i]] = source[key];
         newObj[key] = source[key];
       }
     }
