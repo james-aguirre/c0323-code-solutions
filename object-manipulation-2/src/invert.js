1de223731a5b78add0bb5d1faaf660f987ed5ffd
James Aguirre <jamesaguirre98@gmail.com>
2023-05-04 02:41:23 +0000

    finished defaults function

--- object-manipulation-2/src/invert.js
+++ object-manipulation-2/src/invert.js
@@ -1,1 +1,9 @@
 /* exported invert */
+function invert(source) {
+  const newObj = {};
+  for (const key in source) {
+    const value = source[key];
+    newObj[value] = key;
+  } console.log(newObj);
+  return newObj;
+}
