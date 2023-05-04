6209048120e9f7a123335eb473a9d37c6f510a6d
James Aguirre <jamesaguirre98@gmail.com>
2023-05-04 21:33:59 +0000

    first three algos complete

--- string-manipulation-3/src/reverse-words.js
+++ string-manipulation-3/src/reverse-words.js
@@ -1,1 +1,10 @@
 /* exported reverseWords */
+function reverseWords(string) {
+  const str = string.split(' ');
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    newStr += str[i].split('').reverse().join('') + ' ';
+  }
+  const result = newStr.substring(0, newStr.length - 1);
+  return result;
+}
--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,1 +1,11 @@
 /* exported titleCase */
+// function titleCase(string) {
+//   const str = string.toLowerCase().split(' ');
+//   const newStr = '';
+//   for (let i = 0; i < str.length; i++) {
+//     str[i].split('');
+//     str[i].toLowerCase();
+//     console.log(str[i]);
+//   }
+//   return str;
+// }
