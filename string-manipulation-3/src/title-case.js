ea33f7d14454249d9617e4341a153a8a87dd688d
James Aguirre <jamesaguirre98@gmail.com>
2023-05-04 21:42:44 +0000

    first three algos complete

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,11 +1,14 @@
 /* exported titleCase */
-// function titleCase(string) {
-//   const str = string.toLowerCase().split(' ');
-//   const newStr = '';
-//   for (let i = 0; i < str.length; i++) {
-//     str[i].split('');
-//     str[i].toLowerCase();
-//     console.log(str[i]);
-//   }
-//   return str;
-// }
+function titleCase(string) {
+  const str = string.toLowerCase().split(' ');
+  let newStr = '';
+  for (let i = 0; i < str.length; i++) {
+    str[i].split('');
+    str[i].toLowerCase();
+    if (str[i].length >= 4) {
+      newStr += str[i][0].toUppercase;
+    }
+    console.log(str[i].length);
+  }
+  return newStr;
+}
