192447c4b8db2a7bc6ea5d2919f352e75accb486
James Aguirre <jamesaguirre98@gmail.com>
2023-05-10 03:08:38 +0000

    halfway through title case function

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -3,12 +3,18 @@ function titleCase(string) {
   const str = string.toLowerCase().split(' ');
   let newStr = '';
   for (let i = 0; i < str.length; i++) {
-    str[i].split('');
-    str[i].toLowerCase();
+    console.log(str[i]);
+    if (str[i] === 'javascript') {
+      newStr += 'JavaScript ';
+    } else
     if (str[i].length >= 4) {
-      newStr += str[i][0].toUppercase;
+      newStr += str[i][0].toUpperCase() + str[i].slice(1) + ' ';
+    } else if (str[i] === str[0]) {
+      newStr += str[i][0].toUpperCase() + str[i].slice(1) + ' ';
+    } else if (str[i] === 'api') {
+      newStr += 'API';
+    } else {
+      newStr += str[i] + ' ';
     }
-    console.log(str[i].length);
-  }
-  return newStr;
+  } return newStr.trimEnd();
 }
