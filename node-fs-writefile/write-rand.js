8cb081f1d15253bf65299ba6d8a91ceadbbf6f73
James Aguirre <jamesaguirre98@gmail.com>
2023-05-11 21:51:16 +0000

    .

---
+++ node-fs-writefile/write-rand.js
@@ -0,0 +1,17 @@
+import { writeFile } from 'node:fs/promises';
+
+function randomNum(max) {
+  const num = Math.floor(Math.random() * max);
+  console.log(num);
+  return num;
+}
+async function random() {
+  try {
+    const promise = await writeFile('random.txt', `${randomNum(5)}\n`);
+    return promise;
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+random();
