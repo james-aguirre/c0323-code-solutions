a6230d2d689b1dfc0244fdf450218d68878368a6
James Aguirre <jamesaguirre98@gmail.com>
2023-05-15 20:53:27 +0000

    .

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,24 +1,29 @@
 import express from 'express';
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 const app = express();
 app.use(express.json());
+
 async function readData() {
   const file = await readFile('data.json');
   return await JSON.parse(file);
 }
 const data = await readData();
+async function dataToJson() {
+  const newObj = JSON.stringify(data, null, 2);
+  await writeFile('data.json', newObj);
+}
 
 app.get('/api/notes', (req, res) => {
   const arr = [];
-  for (const key in data) {
-    arr.push(data[key]);
+  for (const key in data.notes) {
+    arr.push(data.notes[key]);
   }
   res.status(200).json(arr);
 });
 
 app.get('/api/notes/:id', (req, res) => {
   const note = req.params.id;
-  if (note < 0) {
+  if (Math.sign(note) !== 1) {
     res.status(400).send({ error: 'id must be a positive integer' });
   } else if (!data.notes[note]) {
     res.status(404).send({ error: 'cannot find note with id ' + note });
@@ -27,15 +32,51 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
-// app.post('/api/notes', (req, res) => {
-//   grades[nextId] = req.body;
-//   grades[nextId].id = nextId++;
-//   res.send(req.body).json(grades[nextId]).status(201);
-// });
-// app.delete('/api/notes/:id', (req, res) => {
-//   delete grades[req.params.id];
-//   res.sendStatus(204);
-// });
+app.post('/api/notes', (req, res) => {
+  if (req.body === undefined) {
+    res.status(400).send({ error: 'content is a required field' });
+  } else if (req === Error) {
+    res.status(500).send({ error: 'An unexpected error occured.' });
+  } else {
+    const newNote = data.nextId;
+    data.notes[newNote] = req.body;
+    data.notes[newNote].id = data.nextId++;
+    dataToJson();
+    res.send(req.body).json(data.notes[newNote]).status(201);
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const note = req.params.id;
+  if (Math.sign(note) !== 1) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (!data.notes[note]) {
+    res.status(404).send({ error: 'cannot find note with id ' + note });
+  } else if (req.body === Error) {
+    res.status(500).send({ error: 'An unexpected error occured.' });
+  } else {
+    delete data.notes[note];
+    dataToJson();
+    res.status(204).send(res.body);
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  const note = req.params.id;
+  if (Math.sign(note) !== 1) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (!data.notes[note]) {
+    res.status(404).send({ error: 'cannot find note with id ' + note });
+  } else if (req.body === Error) {
+    res.status(500).send({ error: 'An unexpected error occured.' });
+  } else if (req.body.content === undefined) {
+    res.status(400).send({ error: 'content field is required' });
+  } else {
+    data.notes[note] = req.body;
+    dataToJson();
+    res.status(204).send(req.body);
+  }
+});
 
 app.listen(8080, () => {
   console.log('Express server listening on port 8080');
