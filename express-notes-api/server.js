69e51122b3e1dd85d4c6650f6ea880ab98db6b46
James Aguirre <jamesaguirre98@gmail.com>
2023-05-15 18:49:26 +0000

    select note functionality

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,21 +1,30 @@
 import express from 'express';
 import { readFile } from 'node:fs/promises';
-
 const app = express();
 app.use(express.json());
-
 async function readData() {
   const file = await readFile('data.json');
   return await JSON.parse(file);
 }
 const data = await readData();
 
-app.get('/api/notes', async (req, res) => {
+app.get('/api/notes', (req, res) => {
   const arr = [];
   for (const key in data) {
     arr.push(data[key]);
   }
   res.status(200).json(arr);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const note = req.params.id;
+  if (note < 0) {
+    res.status(400).send({ error: 'id must be a positive integer' });
+  } else if (!data.notes[note]) {
+    res.status(404).send({ error: 'cannot find note with id ' + note });
+  } else {
+    res.status(200).send(data.notes[note]);
+  }
 });
 
 // app.post('/api/notes', (req, res) => {
