f78cf9bab68f8d9f74cf70b678bf4194374313a6
James Aguirre <jamesaguirre98@gmail.com>
2023-05-22 19:14:03 +0000

    toggleswitch react

---
+++ react-toggle-switch-component/my-app/src/App.js
@@ -0,0 +1,7 @@
+import ToggleSwitch from './ToggleSwitch';
+function App() {
+  return <ToggleSwitch label="on" className="container" />;
+}
+// const bkgColor = isClicked ? color : 'white';
+// <button onClick={handleClick} style={backgroundColor: bkgColor<}
+export default App;
