c7a9a73f3b746108ca6a2d247cf66ac8b4610cae
James Aguirre <jamesaguirre98@gmail.com>
2023-05-23 16:28:28 +0000

    .

--- react-rendering-lists/my-app/src/PokemonList.js
+++ react-rendering-lists/my-app/src/PokemonList.js
@@ -1,8 +1,14 @@
 import { pokedex } from './App';
 
 export default function PokemonList() {
-  const pokemonList = pokedex.map((pokemon) => (
-    <li key={pokemon.id}>{pokemon.name}</li>
-  ));
-  return <ul>{pokemonList}</ul>;
+  // const pokemonList = pokedex.map((pokemon) => (
+  //   <li key={pokemon.id}>{pokemon.name}</li>
+  //  ));
+  return (
+    <ul>
+      {pokedex.map((pokemon) => (
+        <li key={pokemon.id}>{pokemon.name}</li>
+      ))}
+    </ul>
+  );
 }
