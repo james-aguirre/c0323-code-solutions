2b4d4ac7a1c75c33c3c0380ffe3a0b347ac6e540
James Aguirre <jamesaguirre98@gmail.com>
2023-05-25 16:47:37 +0000

    react-app-drawer

--- react-app-drawer-component/my-app/src/DrawerComponent.js
+++ react-app-drawer-component/my-app/src/DrawerComponent.js
@@ -16,10 +16,10 @@ export default function DrawerComponent({ heading, items, onSelect }) {
   return (
     <>
       <FaBars onClick={toggleOpen} className="menu-icon" />
-      <Backdrop isDrawn={isOpen} onClick={toggleOpen} />
+      <Backdrop isActive={isOpen} onClick={toggleOpen} />
       <Menu isOpen={isOpen}>
         <h3 className="menu-heading">{heading}</h3>
-        <ul class="menu-items">
+        <ul className="menu-items">
           {items.map((item) => (
             <li
               key={item}
@@ -34,8 +34,8 @@ export default function DrawerComponent({ heading, items, onSelect }) {
   );
 }
 
-function Backdrop({ isDrawn, onClick }) {
-  const className = isDrawn ? 'menu-shade is-drawn' : 'menu-shade';
+function Backdrop({ isActive, onClick }) {
+  const className = isActive ? 'menu-shade is-drawn' : 'menu-shade';
   return <div className={className} onClick={onClick}></div>;
 }
 
