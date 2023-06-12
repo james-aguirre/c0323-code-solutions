07cb3ff572e9bcf3d49086b44f608b61fd942a6d
James Aguirre <jamesaguirre98@gmail.com>
2023-06-12 21:52:32 +0000

    .

---
+++ javascript-closures/my-app/src/Counter.js
@@ -0,0 +1,18 @@
+// import React, { useEffect, useState, useCallback } from 'react';
+
+// export default function Counter() {
+//   const [counter, setCounter] = useState(0);
+//   useEffect(() => {
+//     fun();
+//     setCounter((prev) => prev + 1);
+//   }, []);
+
+//   const fun = useCallback(() => {
+//     function getData() {
+//       // fetch data
+//       return { foo: 'bar' };
+//     }
+//   });
+
+//   return <div>Fetching {counter} times</div>;
+// }
